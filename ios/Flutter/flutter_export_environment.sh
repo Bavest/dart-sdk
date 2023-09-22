0c15068285ac34a993145812e2f46f9eb05f0879
monolidth <william.todt@mailbox.org>
2023-09-22 10:19:43 +0200

    Upgrade dependencies

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/monolidth/StudioProjects/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/monolidth/StudioProjects/bavest_app/packages/bavest"
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.3.4/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/monolidth/StudioProjects/dart-sdk"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=0.0.6"
+export "FLUTTER_BUILD_NUMBER=0.0.6"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
